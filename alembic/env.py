5d9883c31dac384a80a99699f5ea6177cb22a748
hjiwane <himanshu.jiwane@gmail.com>
2026-02-09 14:12:31 -0500

    Add initial Alembic migration

--- alembic/env.py
+++ alembic/env.py
@@ -6,6 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 from app import models
 from app.config import settings
+from app.database import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,7 +22,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = models.Base.metadata
+target_metadata = Base.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
--- app/main.py
+++ app/main.py
@@ -6,8 +6,8 @@ from .routers import post, user, auth, vote
 from .config import settings
 
 print(settings.database_username)
-
 # models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
